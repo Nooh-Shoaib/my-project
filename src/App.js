7b37ff8a34ad188a8ab4701634c963b7c76d0378
Nooh-Shoaib <147729418+Nooh-Shoaib@users.noreply.github.com>
2023-12-20 19:59:55 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import Header from './Components/Header';
-import SidebarIcons from './Components/SidebarIcons';
-import Footer from './Components/Footer';
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from "./Pages/LandingPage";
+import Error404 from "./Pages/Error404";
 
 
 function App() {
-  return (
-    <div>
-      <Header />
-      {/* <SidebarIcons /> */}
 
-      <Footer />
-    </div>
+  return (
+
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="*" element={<Error404 />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header';
+import SidebarIcons from './SidebarIcons';
+import Footer from './Footer';
+
+
+
+function Layout({ children }) {
+        return (
+                <div>
+                        <Header />
+                        <SidebarIcons />
+                        {children}
+                        <Footer />
+                </div>
+        );
+}
+
+export default Layout;
